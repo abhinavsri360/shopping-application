dcf0eb81a0fa96a4eea25fc90cf72b5949bfd0c8
abhinavsri360 <abhinav.022000@gmail.com>
2019-08-19 19:36:48 +0530

    Application setup

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-7">
+        <div class="row">
+          <div :key="product.id" class="column-md-6" v-for="product in products">
+            <product :isInCart="isInCart(product)" v-on:add-to-cart="addToCart(product)" :product="product"></product>
+          </div>
+        </div>
+      </div>
+        <div class="col-md-5 my-5">
+          <cart v-on:pay="pay()" v-on:remove-from-cart="removeitem($event)" :items="cart"></cart>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import products from '@/products.json';
+import Product from './components/product.vue';
+import Cart from './components/Cart.vue';
+
+export default {
+  name: 'app',
+  components:{
+    Product,
+    Cart
+  },
+  data(){
+    return{
+      products,
+      cart:[]
+    }
+  },
+  methods:{
+    addToCart(product){
+      this.cart.push(product)
+    },
+    isInCart(product){
+      const item = this.cart.find(item => item.id === product.id)
+      if(item)
+      {return true}
+      return false 
+    },
+    removeitem(product){
+      this.cart = this.cart.filter(item => item.id != product.id)
+    },
+    pay(){
+      this.cart = []
+      alert('Congratulations! Your Shopping is done')
+    }
+  }
+}
+</script>
+
+<style>
+  body{
+    background-color: blue;
+  }
+</style>
